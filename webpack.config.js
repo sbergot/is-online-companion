665546104640369f42848d5cf126cb0fa4348658
Simon Bergot <sbergot@talentsoft.com>
2019-07-20 11:35:20 +0200

    configure project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, argv) => ({
+    entry: {
+        main: "./src/index.tsx"
+    },
+
+    devtool: argv.mode === 'production' ? "" : "eval-source-map",
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+    },
+
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+
+    plugins: [
+        new CopyWebpackPlugin([{ from: './static' }]),
+        new CleanWebpackPlugin(),
+    ],
+});
